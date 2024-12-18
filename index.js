efe16355e6eab491222f6c06b9828ae942f70359
mangadilip <mangadileep42@gmail.com>
2024-12-18 17:33:20 +0530

    newfile

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+
+let username=prompt('enter username')
+let user_object={
+    username:username,
+    product:"washing_machine",
+    price:"25000",
+    day:"monday",
+    occassion:"pongal",
+    plafform:"amazon",
+}
+console.log(`${user_object.username} has purchased a ${user_object.product} which costs ${user_object.price} on ${user_object.day} on occassion of ${user_object.occassion} in ${user_object.plafform}`)
+alert(`${user_object.username} has purchased a ${user_object.product} which costs ${user_object.price} on ${user_object.day} on occassion of ${user_object.occassion} in ${user_object.plafform}`)
+
+
+
+let studentname=prompt('enter studentname')
+let student_object={
+    studentname:studentname,
+    companyname:'stick-soft',
+    package:'4lpa',
+    domain:'rectdeveloper',
+    joiningdate:'01/01/2025',
+}
+console.log(`${student_object.studentname} got palced in ${student_object.companyname} company as a ${student_object.domain} with package of ${student_object.package} and joining is on ${student_object.joiningdate}`)
+alert(`${student_object.studentname} got palced in ${student_object.companyname} company as a ${student_object.domain} with package of ${student_object.package} and joining is on ${student_object.joiningdate}`)
